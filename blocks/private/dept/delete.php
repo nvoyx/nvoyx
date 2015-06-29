2942a55053f5ead8458704fcc9b273b06ad29f44
Les <Les@10.10.10.34>
2015-06-29 16:30:57 +0100

    Added dept delete code and redirect

--- blocks/private/dept/delete.php
+++ blocks/private/dept/delete.php
@@ -8,3 +8,16 @@
  *
  */
 
+/*
+ * deletes a dept and redirects to the dept page
+ */
+
+/* delete a dept entry */
+$NVX_DB->DB_CLEAR(array("ALL"));
+$NVX_DB->DB_SET_FILTER("`id`={$NVX_BOOT->FETCH_ENTRY("breadcrumb",3)}");
+$NVX_DB->DB_QUERY("DELETE","FROM `dept`");
+
+/* TODO should really check against the current users and fallback to the default dept where appropriate */
+
+/* redirect to the dept listings */
+$NVX_BOOT->HEADER(array("LOCATION"=>"/settings/dept/list"));
