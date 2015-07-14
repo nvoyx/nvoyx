31f3aa14c5f8d13621c17b8b78a847fae61af5ef
Les <Les@10.10.10.26>
2015-07-14 12:05:44 +0100

    updated link title to restore

--- blocks/private/recovery/list.php
+++ blocks/private/recovery/list.php
@@ -158,7 +158,7 @@ foreach($type as $t){
 					
 				<div class="blank row">
 					<label class="blank fl half"><?=ucwords($c["title"]);?><br><span class="tt"><?=$c["modified"];?> - <?=$c["by"];?></span></label>
-					<a title="edit" href="<?php echo "/settings/recovery/restore/".$t."/".$c["id"];?>"><img class="blank icon fr" src="/settings/resources/files/images/private/group-button-edit.png"></a>
+					<a title="restore" href="<?php echo "/settings/recovery/restore/".$t."/".$c["id"];?>"><img class="blank icon fr" src="/settings/resources/files/images/private/group-button-edit.png"></a>
 				</div>
 
 			<?php }
