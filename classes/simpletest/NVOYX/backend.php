efd59ed36da181a27ba34b137ea40548081a3b1d
Les <Les@10.10.10.23>
2015-07-20 16:39:43 +0100

    added ajax and redirect tests

--- classes/simpletest/NVOYX/backend.php
+++ classes/simpletest/NVOYX/backend.php
@@ -33,16 +33,109 @@ class Backend extends WebTestCase {
 		$this->assertText('SYSTEM');
 	}
 	
+	/* 301 REDIRECT TESTS */
+	
 	function test_301_Redirects_List_Response() {
 		$this->get($this->o['https'].'/settings/redirects/list');
 		$this->assertResponse(200);
 		$this->assertText('301 REDIRECTS');
-	}	
+	}
+	
+	function test_Add_Redirect() {		
+		$this->get($this->o['https'].'/settings/redirects/add');
+		$this->assertText('Old URL');
+		$r=explode('/',$this->getUrl());
+		$this->o['redirect_id']=array(0=>$r[count($r)-1]);
+	}
+	
+	function test_Update_Redirect() {
+		if(is_numeric($this->o['redirect_id'][0])){
+			$this->get($this->o['https'].'/settings/redirects/edit/'.$this->o['redirect_id'][0]);
+			$this->setFieldById('old', '/nvx-redirect-test-a');
+			$this->setFieldById('new', '/contact');
+			$this->clickSubmitById('submit');
+			$this->assertField('old',true,'/nvx-redirect-test-a');
+			$this->assertField('new',true,'/contact');
+		}
+	}
+	
+	function test_Redirect_Reponds_301() {
+		if(is_numeric($this->o['redirect_id'][0])){
+			$this->setMaximumRedirects(0);
+			$this->get($this->o['https'].'/nvx-redirect-test-a');
+			$this->assertResponse(301);
+		}
+	}
+	
+	function test_Redirect_Of_Redirect() {
+		$this->get($this->o['https'].'/settings/redirects/add');
+		$r=explode('/',$this->getUrl());
+		$this->o['redirect_id'][1]=$r[count($r)-1];
+		$this->setFieldById('old', '/nvx-redirect-test-b');
+		$this->setFieldById('new', '/nvx-redirect-test-a');
+		$this->clickSubmitById('submit');
+		$this->setMaximumRedirects(0);
+		$this->get($this->o['https'].'/nvx-redirect-test-b');
+		$this->assertResponse(301);
+	}
+	
+	function test_Delete_Redirects() {
+		if(is_numeric($this->o['redirect_id'][0])){
+			$this->get($this->o['https'].'/settings/redirects/delete/'.$this->o['redirect_id'][0]);
+			$this->get($this->o['https'].'/settings/redirects/delete/'.$this->o['redirect_id'][1]);
+			$this->assertText('301 REDIRECTS');
+		}
+	}
+	
+	function test_404_Redirect_Repsonse() {
+		$this->get($this->o['https'].'/nvx-redirect-test-a');
+		$this->assertResponse(404);
+	}
 
+	/* AJAX TESTS */
 	function test_Ajax_List_Response() {
 		$this->get($this->o['https'].'/settings/ajaxmanager/list');
 		$this->assertResponse(200);
 		$this->assertText('AJAX');
+	}
+	
+	function test_Ajax_System_Files_Available() {
+		$this->get($this->o['https'].'/settings/ajaxmanager/list');
+		$this->assertText('ckbrowse.php');
+		$this->assertText('ckupload.php');
+		$this->assertText('comments.php');
+		$this->assertText('contentfilter.php');
+		$this->assertText('heirarchy.php');
+		$this->assertText('tagbox.php');
+		$this->assertText('upload.php');
+	}
+	
+	function test_Ajax_Folder_Writeable_By_Server() {
+		$this->assertTrue(is_writable($this->o['core'].'/blocks/private/ajax'));
+	}
+	
+	function test_Add_Ajax() {
+		$this->get($this->o['https'].'/settings/ajaxmanager/add');
+		$this->assertText('Name');
+		$r=explode('/',$this->getUrl());
+		$this->o['variable_ajax']=$r[count($r)-1];
+	}
+	
+	function test_Update_Ajax() {
+		if(is_numeric($this->o['variable_ajax'])){
+			$this->get($this->o['https'].'/settings/ajaxmanager/edit/'.$this->o['variable_ajax']);
+			$this->setFieldById('url', 'nvxunit');
+			$this->clickSubmitById('submit');
+			$this->assertField('url',true,'nvxunit');
+			$this->assertTrue(file_exists($this->o['core'].'/blocks/private/ajax/nvxunit.php'));
+		}
+	}
+	
+	function test_Delete_Ajax() {
+		if(is_numeric($this->o['variable_ajax'])){
+			$this->get($this->o['https'].'/settings/ajaxmanager/delete/'.$this->o['variable_ajax']);
+			$this->assertText('AJAX');
+		}
 	}
 	
 	function test_Blocks_List_Response() {
@@ -234,13 +327,6 @@ class Backend extends WebTestCase {
 		$this->assertText('Name');
 		$r=explode('/',$this->getUrl());
 		$this->o['variable_id']=$r[count($r)-1];
-	}
-	
-	function test_Edit_Variable() {
-		if(is_numeric($this->o['variable_id'])){
-			$this->get($this->o['https'].'/settings/variables/edit/'.$this->o['variable_id']);
-			$this->assertText('Name');
-		}
 	}
 	
 	function test_Update_Variable() {
