e296c13bdc143a22bc08e20e9d3dd7067dc032af
nvoyx <nvoyxmail@gmail.com>
2015-08-06 08:35:54 +0100

    added dev tools

--- blocks/public/1.php
+++ blocks/public/1.php
@@ -14,10 +14,20 @@
  * returns admin navigation
  */
 
+if($NVX_USER->GRANTED("s")){
+	if(isset($DEBUG)){
+		if(is_array($DEBUG)){
+			$DEBUG=$NVX_HTML->UL($DEBUG);
+		}
+	} else {
+		$DEBUG = 'pass an array or variable to $DEBUG to view here.';
+	}
+}
+
 ?>
 
 <div id="admin" class="hide">
-	<div>
+	<div id='admin-nav'>
 		<img class='fl'src='/settings/resources/files/images/public/header-client.png' height='24' >
 		<a href="/settings/content/list">Admin</a> | 
 		<?php if($NVX_DEPT->GRANTED($NVX_USER->FETCH_ARRAY()['dept'],$PAGE['tid'])){ ?>
@@ -25,4 +35,27 @@
 		<?php } ?>
 		<a href="/settings/user/logout">Logout</a>
 	</div>
+	<?php if($NVX_USER->GRANTED("s")){ ?>
+	<div class="row pad-t20">
+		<div class='col all100'>
+			<p class='fs12 pad-b15'>
+				Memory usage: <?=implode(' ',$NVX_BOOT->HUMAN_FILESIZE(memory_get_peak_usage()));?><br>
+				Db usage: <?=$NVX_DB->CALLS();?> calls<br>
+				Execution time: <?=number_format(microtime(true)-$NVX_BOOT->FETCH_ENTRY('microstamp'),5);?> seconds
+			</p>
+			<p class='fs12 pad-b0'>
+				Page:
+			</p>
+			<div class='col all100 fs12 pad-lr20 pad-tb10 mar-b15' style='background-color:#f8f8f8;max-height:200px;overflow:scroll;'>
+				<?=$NVX_HTML->UL($PAGE);?>
+			</div>
+			<p class='fs12 pad-b0'>
+				Debug:
+			</p>
+			<div class='col all100 fs12 pad-lr20 pad-tb10 mar-b15' style='background-color:#f8f8f8;max-height:200px;overflow:scroll;'>
+				<?=$DEBUG;?>
+			</div>
+		</div>
+	</div>
+	<?php } ?>
 </div>
