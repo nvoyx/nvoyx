1f138297ac6b51de189409e3c5e2cb30ffb2b1bf
nvoyx <nvoyxmail@gmail.com>
2015-08-26 17:13:17 +0100

    defined several text effects

---
+++ resources/js/ckstyles.js
@@ -0,0 +1,176 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+CKEDITOR.stylesSet.add( 'nvx', [
+	{
+		name: 'Sub',
+		element: 'h2',
+		attributes:{
+			'class':'nv-nofx'
+		}
+	},
+	{
+		name: 'Sub (fade)',
+		element: 'h2',
+		attributes: {
+			'class':'fadeIn nv-fx'
+		}
+	},
+	{
+		name: 'Sub (fade left)',
+		element: 'h2',
+		attributes: {
+			'class':'fadeInLeft nv-fx'
+		}
+	},
+	{
+		name: 'Sub (fade right)',
+		element: 'h2',
+		attributes: {
+			'class':'fadeInRight nv-fx'
+		}
+	},
+	{
+		name: 'Sub (fade up)',
+		element: 'h2',
+		attributes: {
+			'class':'fadeInUp nv-fx'
+		}
+	},
+	{
+		name: 'Sub (fade down)',
+		element: 'h2',
+		attributes: {
+			'class':'fadeInDown nv-fx'
+		}
+	},
+	{
+		name: 'Sub (stretch)',
+		element: 'h2',
+		attributes: {
+			'class':'nv-fx rubberBand'
+		}
+	},
+	{
+		name: 'Sub (zoom)',
+		element: 'h2',
+		attributes: {
+			'class':'nv-fx zoomIn'
+		}
+	},
+	{
+		name: 'SubSub',
+		element: 'h3',
+		attributes: {
+			'class':'nv-nofx'
+		}
+	},
+	{
+		name: 'SubSub (fade)',
+		element: 'h3',
+		attributes: {
+			'class':'fadeIn nv-fx'
+		}
+	},
+	{
+		name: 'SubSub (fade left)',
+		element: 'h3',
+		attributes: {
+			'class':'fadeInLeft nv-fx'
+		}
+	},
+	{
+		name: 'SubSub (fade right)',
+		element: 'h3',
+		attributes: {
+			'class':'fadeInRight nv-fx'
+		}
+	},
+	{
+		name: 'SubSub (fade up)',
+		element: 'h3',
+		attributes: {
+			'class':'fadeInUp nv-fx'
+		}
+	},
+	{
+		name: 'SubSub (fade down)',
+		element: 'h3',
+		attributes: {
+			'class':'fadeInDown nv-fx'
+		}
+	},
+	{
+		name: 'SubSub (stretch)',
+		element: 'h3',
+		attributes: {
+			'class':'nv-fx rubberBand'
+		}
+	},
+	{
+		name: 'SubSub (zoom)',
+		element: 'h3',
+		attributes: {
+			'class':'nv-fx zoomIn'
+		}
+	},
+	{
+		name: 'Para',
+		element: 'p',
+		attributes: {
+			'class':'nv-nofx'
+		}
+	},
+	{
+		name: 'Para (fade)',
+		element: 'p',
+		attributes: {
+			'class':'fadeIn nv-fx'
+		}
+	},
+	{
+		name: 'Para (fade left)',
+		element: 'p',
+		attributes: {
+			'class':'fadeInLeft nv-fx'
+		}
+	},
+	{
+		name: 'Para (fade right)',
+		element: 'p',
+		attributes: {
+			'class':'fadeInRight nv-fx'
+		}
+	},
+	{
+		name: 'Para (fade up)',
+		element: 'p',
+		attributes: {
+			'class':'fadeInUp nv-fx'
+		}
+	},
+	{
+		name: 'Para (fade down)',
+		element: 'p',
+		attributes: {
+			'class':'fadeInDown nv-fx'
+		}
+	},
+	{
+		name: 'Para (stretch)',
+		element: 'p',
+		attributes: {
+			'class':'nv-fx rubberBand'
+		}
+	},
+	{
+		name: 'Para (zoom)',
+		element: 'p',
+		attributes: {
+			'class':'nv-fx zoomIn'
+		}
+	}
+]);
