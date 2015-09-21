1867c60851aecd2db63d07fdfdac34e34c2341c2
nvoyx <nvoyxmail@gmail.com>
2015-09-21 10:21:41 +0100

    updated class and method names

--- blocks/private/recovery/restore.php
+++ blocks/private/recovery/restore.php
@@ -13,35 +13,35 @@
  */
 
 /* grab the page type */
-$tid = $NVX_BOOT->FETCH_ENTRY("breadcrumb",3);
+$tid = $nvBoot->fetch_entry("breadcrumb",3);
 
 /* grab the page node */
-$nid = $NVX_BOOT->FETCH_ENTRY("breadcrumb",4);
+$nid = $nvBoot->fetch_entry("breadcrumb",4);
 
 /* grab the system data from the archive */
-$data=file_get_contents("zip://".$NVX_BOOT->FETCH_ENTRY("recovery")."/".$tid."/".$nid.".zip"."#record/script/db.json");
+$data=file_get_contents("zip://".$nvBoot->fetch_entry("recovery")."/".$tid."/".$nid.".zip"."#record/script/db.json");
 
 /* decrypt the data */
-$data = $NVX_BOOT->JSON($data,"decode");
+$data = $nvBoot->json($data,"decode");
 
 /* create an updated modified date entry */
-$mod = date('Y-m-d H:i:00',$NVX_BOOT->FETCH_ENTRY("timestamp"));
+$mod = date('Y-m-d H:i:00',$nvBoot->fetch_entry("timestamp"));
 
 /* ttp and ttc are sometimes NULL, which is returned by the db as an empty string - the following fixes that */
 if($data["node"]["ttp"]==""){$data["node"]["ttp"]="NULL";}
 if($data["node"]["ttc"]==""){$data["node"]["ttc"]="NULL";}
 
 /* update the node */
-$NVX_DB->CLEAR(array("ALL"));
-$NVX_DB->SET_FILTER("`page`.`id`={$nid}");
-$NVX_DB->QUERY("INSERT","INTO `page` (`id`,`tid`,`title`,`heading`,`teaser`,`alias`,`description`,`body`,`importance`,`sttp`,`sttc`,`published`,`ttp`,`ttc`,`date`,`modified`,`by`) " .
+$nvDb->clear(array("ALL"));
+$nvDb->set_filter("`page`.`id`={$nid}");
+$nvDb->query("INSERT","INTO `page` (`id`,`tid`,`title`,`heading`,`teaser`,`alias`,`description`,`body`,`importance`,`sttp`,`sttc`,`published`,`ttp`,`ttc`,`date`,`modified`,`by`) " .
 		"VALUES ({$nid},{$tid},'{$data["node"]["title"]}','{$data["node"]["heading"]}','{$data["node"]["teaser"]}','{$data["node"]["alias"]}',".
 		"'{$data["node"]["description"]}','{$data["node"]["body"]}',{$data["node"]["importance"]},{$data["node"]["sttp"]},{$data["node"]["sttc"]},".
 		"{$data["node"]["published"]},{$data["node"]["ttp"]},{$data["node"]["ttc"]},'{$data["node"]["date"]}','{$mod}',{$_SESSION["id"]})");
 		
 /* connect to the zip archive */
 $zip = new \ZipArchive;
-$zip->open($NVX_BOOT->FETCH_ENTRY("recovery")."/".$tid."/".$nid.".zip");
+$zip->open($nvBoot->fetch_entry("recovery")."/".$tid."/".$nid.".zip");
 
 /* array of tables */
 $dtypes = array(
@@ -67,8 +67,8 @@ foreach($dtypes as $dt){
 		foreach($data[$dt] as $d){
 			
 			/* put the entry into the database */
-			$NVX_DB->CLEAR(array("ALL"));
-			$NVX_DB->QUERY("INSERT","INTO `{$dt}` (`id`,`nid`,`gid`,`vid`,`pid`,`fid`,`values`) " . 
+			$nvDb->clear(array("ALL"));
+			$nvDb->query("INSERT","INTO `{$dt}` (`id`,`nid`,`gid`,`vid`,`pid`,`fid`,`values`) " . 
 							"VALUES ({$d["id"]},{$d["nid"]},{$d["gid"]},{$d["vid"]},{$d["pid"]},{$d["fid"]},'{$d["values"]}')");
 							
 			switch ($dt):
@@ -76,7 +76,7 @@ foreach($dtypes as $dt){
 				case "filelist":
 					
 					/* decode the entry */
-					$d["values"] = $NVX_BOOT->JSON(stripslashes($d["values"]),"decode");
+					$d["values"] = $nvBoot->json(stripslashes($d["values"]),"decode");
 					
 					/* cycle through the entries to grab the file reference */
 					foreach($d["values"] as $f){
@@ -84,8 +84,8 @@ foreach($dtypes as $dt){
 						
 						/* check we have a valid file name */
 						if($f["name"]!=""){
-							$zip->extractTo($NVX_BOOT->FETCH_ENTRY("documents")."/","record/documents/".$f["name"]);
-							rename($NVX_BOOT->FETCH_ENTRY("documents")."/record/documents/".$f["name"],$NVX_BOOT->FETCH_ENTRY("documents")."/".$f["name"]);
+							$zip->extractTo($nvBoot->fetch_entry("documents")."/","record/documents/".$f["name"]);
+							rename($nvBoot->fetch_entry("documents")."/record/documents/".$f["name"],$nvBoot->fetch_entry("documents")."/".$f["name"]);
 						}
 					}
 					break;
@@ -93,7 +93,7 @@ foreach($dtypes as $dt){
 				case "imagelist":
 					
 					/* decode the entry */
-					$d["values"] = $NVX_BOOT->JSON(stripslashes($d["values"]),"decode");
+					$d["values"] = $nvBoot->json(stripslashes($d["values"]),"decode");
 					
 					/* cycle through the entries to grab the file reference */
 					foreach($d["values"] as $f){
@@ -101,8 +101,8 @@ foreach($dtypes as $dt){
 						
 						/* check we have a valid file name */
 						if($f["name"]!=""){
-							$zip->extractTo($NVX_BOOT->FETCH_ENTRY("images")."/","record/cms/".$f["name"]);
-							rename($NVX_BOOT->FETCH_ENTRY("images")."/record/cms/".$f["name"],$NVX_BOOT->FETCH_ENTRY("images")."/".$f["name"]);							
+							$zip->extractTo($nvBoot->fetch_entry("images")."/","record/cms/".$f["name"]);
+							rename($nvBoot->fetch_entry("images")."/record/cms/".$f["name"],$nvBoot->fetch_entry("images")."/".$f["name"]);							
 						}
 					}
 					break;
@@ -113,12 +113,12 @@ foreach($dtypes as $dt){
 }
 
 /* directory cleanup */
-if(file_exists($NVX_BOOT->FETCH_ENTRY("documents")."/record/documents")){
-	$NVX_BOOT->DEL_TREE($NVX_BOOT->FETCH_ENTRY("documents")."/record");
+if(file_exists($nvBoot->fetch_entry("documents")."/record/documents")){
+	$nvBoot->del_tree($nvBoot->fetch_entry("documents")."/record");
 }
 
-if(file_exists($NVX_BOOT->FETCH_ENTRY("images")."/record/cms")){
-	$NVX_BOOT->DEL_TREE($NVX_BOOT->FETCH_ENTRY("images")."/record");
+if(file_exists($nvBoot->fetch_entry("images")."/record/cms")){
+	$nvBoot->del_tree($nvBoot->fetch_entry("images")."/record");
 }
 
 
@@ -126,17 +126,17 @@ if(file_exists($NVX_BOOT->FETCH_ENTRY("images")."/record/cms")){
 $zip->close();
 
 /* check that a rollback directory doesn't exist for this page */
-if(!file_exists($NVX_BOOT->FETCH_ENTRY("rollback")."/".$nid)){
+if(!file_exists($nvBoot->fetch_entry("rollback")."/".$nid)){
 	
 	/* make the rollback directory for this page */
-	mkdir($NVX_BOOT->FETCH_ENTRY("rollback")."/".$nid);
+	mkdir($nvBoot->fetch_entry("rollback")."/".$nid);
 }
 
 /* copy the rollback to a new archive */
-copy($NVX_BOOT->FETCH_ENTRY("recovery")."/".$tid."/".$nid.".zip",$NVX_BOOT->FETCH_ENTRY("rollback")."/".$nid."/".$NVX_BOOT->FETCH_ENTRY("timestamp").".zip");
+copy($nvBoot->fetch_entry("recovery")."/".$tid."/".$nid.".zip",$nvBoot->fetch_entry("rollback")."/".$nid."/".$nvBoot->fetch_entry("timestamp").".zip");
 
 /* delete the recovery file */
-unlink($NVX_BOOT->FETCH_ENTRY("recovery")."/".$tid."/".$nid.".zip");
+unlink($nvBoot->fetch_entry("recovery")."/".$tid."/".$nid.".zip");
 
 /* issue a notification */
 $_SESSION['notify']=array(
@@ -145,4 +145,4 @@ $_SESSION['notify']=array(
 );
 
 /* redirect to the restored content-edit */
-$NVX_BOOT->HEADER(array("LOCATION"=>"/settings/content/edit/{$nid}"));
+$nvBoot->header(array("LOCATION"=>"/settings/content/edit/{$nid}"));
